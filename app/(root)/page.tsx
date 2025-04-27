9c01b0c661ae209ccab7ec4a3b933218cd4fc074
Muhammad Haider Sheikh <Haidersheikh243@gmail.com>
2025-04-27 12:04:35 +0500

    initial landing page design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,26 @@
+import BrandSlide from "@/components/components_landing/BrandSlide"
+import CTA from "@/components/components_landing/CTA"
+import Footer from "@/components/components_landing/Footer"
+import Header from "@/components/components_landing/Header"
+import Hero from "@/components/components_landing/Hero"
+import Pricing from "@/components/components_landing/Pricing"
+import ProductCard from "@/components/components_landing/ProductCard"
+import ProductShowcase from "@/components/components_landing/ProductShowcase"
+import Testimonials from "@/components/components_landing/Testimonials"
 
 
 const page = () => {
   return (
     <div>
-      Landing Page
+      <Header />
+      <Hero/>
+      <BrandSlide/>
+      <ProductShowcase/>
+      <ProductCard/>
+      <Pricing/>
+      <Testimonials/>
+      <CTA/>
+      <Footer/>
     </div>
   )
 }
